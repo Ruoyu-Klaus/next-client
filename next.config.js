88c871862c72cbbf150bad5b7801d63347f871dd
RuoyuKlaus <klaus1201810802@gmail.com>
2021-10-22 13:07:17 +0800

    💚 fix webpack error for jsdom

--- next.config.js
+++ next.config.js
@@ -4,9 +4,12 @@ const nextConfig = {
   webpack5: false,
   target: 'serverless',
   experimental: { esmExternals: true },
-  // reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL || 'http://127.0.0.1:7001/v1/',
+  },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin(/canvas/, /jsdom$/));
+    return config;
   },
 };
 
