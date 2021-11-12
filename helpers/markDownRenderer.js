d69b9c57d6136c3d2b71bacf130a936a82dda538
ruoyuklaus <klaus1201810802@gmail.com>
2021-11-12 14:12:10 +0800

    🎨 clern and refactor markdown render

--- helpers/markDownRenderer.js
+++ helpers/markDownRenderer.js
@@ -1,51 +1,55 @@
-import marked from 'marked';
-import hljs from 'highlight.js';
-import MarkDownTOC from './MarkDownTOC';
+import marked from 'marked'
+import hljs from 'highlight.js'
+import MarkDownTOC from './MarkDownTOC'
 
-import createDOMPurify from 'dompurify';
-import { JSDOM } from 'jsdom';
+import createDOMPurify from 'dompurify'
+import { JSDOM } from 'jsdom'
+
+const markdedOptions = {
+  tables: true,
+  breaks: false,
+  smartLists: true,
+  smartypants: false,
+  langPrefix: 'hljs language-',
+  highlight: function (code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+    return hljs.highlight(language, code).value
+  },
+}
 
 export async function getParsedContentWithTocTree(content) {
-  const renderer = new marked.Renderer();
-  const tocRenderer = new MarkDownTOC();
-
+  const renderer = new marked.Renderer()
+  const tocRenderer = new MarkDownTOC()
   renderer.heading = function (text, level) {
-    return tocRenderer.renderHTML(text, level);
-  };
+    return tocRenderer.renderHTML(text, level)
+  }
+  marked.setOptions(markdedOptions)
+  marked.use({ renderer })
 
-  marked.setOptions({
-    tables: true,
-    breaks: false,
-    smartLists: true,
-    smartypants: false,
-    langPrefix: 'hljs language-',
-    highlight: function (code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(language, code).value;
-    },
-  });
-  marked.use({ renderer });
+  const parsedContent = await marked(content)
+  const santizedContent = await sanitizeContent(parsedContent)
+  const tocTree = getTocTree(tocRenderer)
 
-  const parsedContent = await marked(content);
+  return { santizedContent, tocTree }
+}
 
-  const tocTree = getTocTree(tocRenderer);
-
-  const window = new JSDOM('').window;
-  const DOMPurify = createDOMPurify(window);
+async function sanitizeContent(parsedContent) {
+  const window = new JSDOM('').window
+  const DOMPurify = createDOMPurify(window)
   const sanitizeOptions = {
     ADD_TAGS: ['iframe'],
     ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling'],
-  };
+  }
   const santizedContent = await DOMPurify.sanitize(
     parsedContent,
     sanitizeOptions
-  );
-  return { santizedContent, tocTree };
+  )
+  return santizedContent
 }
 
 export function getTocTree(tocRenderer) {
   if (tocRenderer && tocRenderer instanceof MarkDownTOC) {
-    return tocRenderer.getTocItems();
+    return tocRenderer.getTocItems()
   }
-  return null;
+  return null
 }
