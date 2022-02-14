8de7e6d77eb739c53126f8567f593ab4f0813f9f
ruoyuklaus <klaus1201810802@gmail.com>
2022-02-14 15:37:58 +0800

    :rocket: deploy config

--- hooks/usePaginationPost.js
+++ hooks/usePaginationPost.js
@@ -1,58 +1,53 @@
-import { useCallback, useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import { getFilteredData } from "../helpers";
+import {useCallback, useEffect, useState} from 'react'
+import PropTypes from 'prop-types'
+import {getFilteredData} from '../helpers'
 
 usePaginationPost.propTypes = {
-  pageNum: PropTypes.number,
-  limit: PropTypes.number,
-  enableSearch: PropTypes.bool,
-  originalPosts: PropTypes.array,
-};
-
-function usePaginationPost(props) {
-  const {
-    pageNum = 1,
-    limit = 6,
-    enableSearch = false,
-    originalPosts = [],
-  } = props;
-
-  const [hasMore, setHasMore] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
-  const [posts, setPosts] = useState([]);
-  const [query, changSearchValue] = useState("");
-
-  const calculatePagination = (filteredPosts) => {
-    const count = filteredPosts.length;
-    const maxPages = count / limit;
-    if (pageNum >= maxPages) {
-      setHasMore(false);
-    }
-    return filteredPosts.splice((pageNum - 1) * (limit * (pageNum - 1)), limit);
-  };
-
-  const getPostsByPage = useCallback(() => {
-    const filteredPosts = getFilteredData(originalPosts, enableSearch, query);
-    if (filteredPosts.length === 0) {
-      return;
-    }
-    const data = calculatePagination(filteredPosts);
-    setPosts((pre) => [...pre, ...data]);
-  }, [pageNum, originalPosts, query, enableSearch]);
-
-  const clearDataOnQueryChange = () => {
-    setPosts([]);
-    setHasMore(true);
-    setIsLoading(false);
-  };
-
-  useEffect(clearDataOnQueryChange, [query]);
-
-  useEffect(() => {
-    getPostsByPage();
-  }, [pageNum, query]);
-
-  return { posts, isLoading, hasMore, changeSearchValue: changSearchValue };
+    pageNum: PropTypes.number,
+    limit: PropTypes.number,
+    enableSearch: PropTypes.bool,
+    originalPosts: PropTypes.array,
 }
 
-export default usePaginationPost;
+function usePaginationPost(props) {
+    const {pageNum = 1, limit = 6, enableSearch = false, originalPosts = []} = props
+
+    const [hasMore, setHasMore] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
+    const [posts, setPosts] = useState([])
+    const [query, changeSearchValue] = useState('')
+
+    const calculatePagination = (filteredPosts) => {
+        const count = filteredPosts.length
+        const maxPages = count / limit
+        if (pageNum >= maxPages) {
+            setHasMore(false)
+        }
+        return filteredPosts.splice((pageNum - 1) * (limit * (pageNum - 1)), limit)
+    }
+
+    const getPostsByPage = useCallback(() => {
+        const filteredPosts = getFilteredData(originalPosts, enableSearch, query)
+        if (filteredPosts.length === 0) {
+            return
+        }
+        const data = calculatePagination(filteredPosts)
+        setPosts((pre) => [...pre, ...data])
+    }, [pageNum, originalPosts, query, enableSearch])
+
+    const clearDataOnQueryChange = () => {
+        setPosts([])
+        setHasMore(true)
+        setIsLoading(false)
+    }
+
+    useEffect(clearDataOnQueryChange, [query])
+
+    useEffect(() => {
+        getPostsByPage()
+    }, [pageNum, query])
+
+    return {posts, isLoading, hasMore, changeSearchValue}
+}
+
+export default usePaginationPost
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withPlugins = require("next-compose-plugins");
+const withPlugins = require('next-compose-plugins')
 // const withTM = require('next-transpile-modules')([
 //   'three',
 //   'react-three-fiber',
@@ -6,25 +6,24 @@ const withPlugins = require("next-compose-plugins");
 // ])
 
 const nextConfig = {
-  target: "serverless",
-  env: {
-    BASE_URL: process.env.BASE_URL || "http://127.0.0.1:7001/v1/",
-  },
-  webpack5: false,
-  webpack: (config, { webpack, isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader",
-    });
-    config.plugins.push(new webpack.IgnorePlugin(/canvas/, /jsdom$/));
-    return config;
-  },
-};
+    target: 'serverless',
+    env: {
+        BASE_URL: process.env.BASE_URL || 'http://127.0.0.1:7001/v1/',
+    },
+    webpack5: false,
+    webpack: (config, {isServer}) => {
+        if (!isServer) {
+            config.node = {
+                fs: 'empty',
+            }
+        }
+        config.module.rules.push({
+            test: /\.md$/,
+            use: 'raw-loader',
+        })
+        return config
+    },
+}
 
-const plugins = [];
-module.exports = withPlugins(plugins, nextConfig);
+const plugins = []
+module.exports = withPlugins(plugins, nextConfig)
