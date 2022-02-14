838a3c484f7b9b32f448ff2fc0f9c0653f346dc1
RuoyuKlaus <klaus1201810802@gmail.com>
2022-02-14 11:23:37 +0800

    💄 redesign image dispaly

--- pages/blog/post/[cname]/[...slug].js
+++ pages/blog/post/[cname]/[...slug].js
@@ -1,9 +1,4 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Head from "next/head";
-import dayjs from "dayjs";
 import {
-  AspectRatio,
   Box,
   Container,
   Flex,
@@ -13,81 +8,82 @@ import {
   Tag,
   Text,
   VStack,
-} from "@chakra-ui/react";
-
-import BackToTop from "../../../../components/BackToTop";
-import CustomDivider from "../../../../components/CustomDivider";
-import LinkToPost from "../../../../components/LinkToPost";
-import Toc from "../../../../components/Toc";
-
-import { BlogCollection, randomEmoji } from "../../../../helpers";
-import BlogLayout from "../../../../layout/BlogLayout";
-import {
-  NEXT_POST_LABEL,
-  PREVIOUS_POST_LABEL,
-} from "../../../../utils/content";
+} from '@chakra-ui/react'
+import dayjs from 'dayjs'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import BackToTop from '../../../../components/BackToTop'
+import CustomDivider from '../../../../components/CustomDivider'
+import LinkToPost from '../../../../components/LinkToPost'
+import Toc from '../../../../components/Toc'
+import { BlogCollection, randomEmoji } from '../../../../helpers'
+import BlogLayout from '../../../../layout/BlogLayout'
+import { NEXT_POST_LABEL, PREVIOUS_POST_LABEL } from '../../../../utils/content'
 
 function Post({ post = {}, previousPath, nextPath }) {
-  const router = useRouter();
-  const [emoji] = useState(randomEmoji());
+  const router = useRouter()
+  const [emoji] = useState(randomEmoji())
 
-  const { id, title, coverImage, tags, category, content, date, tocTree } =
-    post;
+  const { id, title, coverImage, tags, category, content, date, tocTree } = post
   // const { cname, slug } = router.searchValue
   // const id = slug[0]
 
   useEffect(() => {
-    !id && router.push("/blog");
-  }, []);
+    !id && router.push('/blog')
+  }, [])
 
   return (
     <>
       <Head>
         <title>{title} | Ruoyu</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Container maxW="container.xl" my={8}>
-        <CustomDivider my={4} text={emoji} dividerWidth="full" />
-        <VStack spacing="4">
-          <AspectRatio w={["90%", "80vw", "60vw"]} maxW="600px" ratio={3 / 2}>
-            <Image objectFit="cover" src={coverImage} />
-          </AspectRatio>
+      <Container maxW='container.xl' my={8}>
+        <CustomDivider my={4} text={emoji} dividerWidth='full' />
+        <VStack spacing='4'>
+          <Image
+            w={['90%', '80vw', '60vw']}
+            maxW='600px'
+            objectFit='contain'
+            src={coverImage}
+          />
           <HStack spacing={4}>
-            {tags?.map((tag) => (
+            {tags?.map(tag => (
               <Tag key={tag}>{tag}</Tag>
             ))}
           </HStack>
-          <Box w={["90%", "80vw", "60vw"]} maxW="1000px">
+          <Box w={['90%', '80vw', '60vw']} maxW='1000px'>
             <Heading>{title}</Heading>
           </Box>
 
-          <Flex w={["90%", "80vw", "60vw"]} maxW="1000px" gap={4}>
-            <Text fontSize="0.8rem">{category}</Text>
-            <Text fontSize="0.8rem">{dayjs(date).format("MM-DD, YYYY")}</Text>
+          <Flex w={['90%', '80vw', '60vw']} maxW='1000px' gap={4}>
+            <Text fontSize='0.8rem'>{category}</Text>
+            <Text fontSize='0.8rem'>{dayjs(date).format('MM-DD, YYYY')}</Text>
           </Flex>
 
           <Box
-            w={["90%", "80vw", "60vw"]}
-            maxW="1000px"
-            className="markdown-body"
+            w={['90%', '80vw', '60vw']}
+            maxW='1000px'
+            className='markdown-body'
           >
             <Toc tocTree={tocTree} />
           </Box>
 
           <Box
-            w={["90%", "80vw", "60vw"]}
-            maxW="1000px"
-            className="markdown-body"
-            id="content"
+            w={['90%', '80vw', '60vw']}
+            maxW='1000px'
+            className='markdown-body'
+            id='content'
             dangerouslySetInnerHTML={{ __html: content }}
           />
-          <CustomDivider my={4} text={emoji} dividerWidth="full" />
+          <CustomDivider my={4} text={emoji} dividerWidth='full' />
 
           <Flex
-            w={["auto", "full"]}
-            justifyContent={["center", "space-between"]}
-            flexDir={["column", "row"]}
+            w={['auto', 'full']}
+            justifyContent={['center', 'space-between']}
+            flexDir={['column', 'row']}
           >
             <LinkToPost payload={previousPath} />
             <LinkToPost payload={nextPath} />
@@ -96,35 +92,35 @@ function Post({ post = {}, previousPath, nextPath }) {
         <BackToTop />
       </Container>
     </>
-  );
+  )
 }
 
 export async function getStaticProps(context) {
   try {
-    const { params } = context;
-    const category_name = params.cname;
-    const [id] = params.slug;
+    const { params } = context
+    const category_name = params.cname
+    const [id] = params.slug
 
-    const blogCollection = new BlogCollection();
-    const linkPaths = blogCollection.getAllPostPaths(true);
-    const post = blogCollection.getBlogByCategoryAndId(category_name, id);
+    const blogCollection = new BlogCollection()
+    const linkPaths = blogCollection.getAllPostPaths(true)
+    const post = blogCollection.getBlogByCategoryAndId(category_name, id)
 
     if (!post) {
       return {
         props: { post: {} },
-      };
+      }
     }
 
-    const currentPathIndex = linkPaths.findIndex((path) => path.id === post.id);
+    const currentPathIndex = linkPaths.findIndex(path => path.id === post.id)
     let previousPath = null,
-      nextPath = null;
+      nextPath = null
     if (currentPathIndex !== 0) {
-      previousPath = linkPaths[currentPathIndex - 1];
-      previousPath.label = PREVIOUS_POST_LABEL;
+      previousPath = linkPaths[currentPathIndex - 1]
+      previousPath.label = PREVIOUS_POST_LABEL
     }
     if (currentPathIndex !== linkPaths.length - 1) {
-      nextPath = linkPaths[currentPathIndex + 1];
-      nextPath.label = NEXT_POST_LABEL;
+      nextPath = linkPaths[currentPathIndex + 1]
+      nextPath.label = NEXT_POST_LABEL
     }
 
     return {
@@ -133,35 +129,35 @@ export async function getStaticProps(context) {
         previousPath,
         nextPath,
       },
-    };
+    }
   } catch (error) {
     return {
       props: {
-        msg: "server error",
+        msg: 'server error',
         post: {},
       },
-    };
+    }
   }
 }
 
 export async function getStaticPaths() {
   try {
-    const blogCollection = new BlogCollection();
-    const paths = blogCollection.getAllPostPaths();
+    const blogCollection = new BlogCollection()
+    const paths = blogCollection.getAllPostPaths()
     return {
       paths: paths,
       fallback: false,
-    };
+    }
   } catch (error) {
     return {
       paths: [],
       fallback: true,
-    };
+    }
   }
 }
 
 Post.getLayout = function getLayout(page, categories) {
-  return <BlogLayout categories={categories}>{page}</BlogLayout>;
-};
+  return <BlogLayout categories={categories}>{page}</BlogLayout>
+}
 
-export default Post;
+export default Post
