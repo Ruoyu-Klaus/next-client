c1cbe75bd9b472fef90a19bc56da28ed6198b7a0
ruoyuklaus <klaus1201810802@gmail.com>
2022-05-07 18:02:42 +0800

    :bug: fix: custom cursor dot not work properly

--- components/CustomCursor.js
+++ components/CustomCursor.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState, useRef, useContext} from 'react'
+import React, {useContext, useEffect, useRef, useState} from 'react'
 import useMousePosition from '../hooks/useMousePosition'
 import {CursorContext} from '../context/cursor/CursorContext'
 import {isMobile} from 'react-device-detect'
 
-const getWindowSize = (dom) => {
-    return [dom.offsetWidth, dom.offsetHeight]
+const getElementWidthAndHeight = (element) => {
+    return [element.offsetWidth, element.offsetHeight]
 }
 
 function CustomCursor() {
@@ -18,8 +18,8 @@ function CustomCursor() {
     const {cursorType} = useContext(CursorContext)
     const [isActive, setIsActive] = useState(false)
 
-    const [ringWidth, ringHeight] = ringRef.current ? getWindowSize(ringRef.current) : [0, 0]
-    const [dotWidth, dotHeight] = dotRef.current ? getWindowSize(dotRef.current) : [0, 0]
+    const [ringWidth, ringHeight] = ringRef.current ? getElementWidthAndHeight(ringRef.current) : [0, 0]
+    const [dotWidth, dotHeight] = dotRef.current ? getElementWidthAndHeight(dotRef.current) : [0, 0]
 
     const mouseDownHandler = () => {
         setIsActive(true)
@@ -37,22 +37,24 @@ function CustomCursor() {
             document.removeEventListener('mouseup', mouseUpHandler)
         }
     }, [])
-
-    useEffect(() => {
-        if (ringRef.current) {
-            ringRef.current.style.transform = `translate(${x - ringWidth / 2}px, ${y - ringHeight / 2}px) scale(${isActive ? 1.5 : 1})`
-        }
-        if (dotRef.current) {
-            dotRef.current.style.transform = `translate(${x - dotWidth / 2}px, ${y - dotHeight / 2}px)`
-        }
-    }, [x, y, isActive])
-
     return (
         <>
-            <div ref={ringRef} className="ringContainer">
+            <div
+                ref={ringRef}
+                style={{
+                    transform: `translate(${x - ringWidth / 2}px, ${y - ringHeight / 2}px) scale(${isActive ? 1.5 : 1})`,
+                }}
+                className="ringContainer"
+            >
                 <div className="ring" />
             </div>
-            <div ref={dotRef} className={'dot ' + cursorType} />
+            <div
+                ref={dotRef}
+                style={{
+                    transform: `translate(${x - dotWidth / 2}px, ${y - dotHeight / 2}px)`,
+                }}
+                className={'dot ' + cursorType}
+            />
         </>
     )
 }
