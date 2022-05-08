959486f72f0d318439093ee6632d265f1d039c78
ruoyuklaus <klaus1201810802@gmail.com>
2022-05-08 17:52:17 +0800

    :recycle: refactor: rewrite logic for blog details page

--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,5 @@
 import emojiList from './emoji.json'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import dayjs from 'dayjs'
-import _ from 'lodash'
-import Category from './entity/Category'
-import {Blog} from './entity/Blog'
 import {getParsedContentWithTocTree} from './markDownRenderer'
-
 import blogCollection from '../_posts/blogCollection.json'
 
 export const getAllBlogs = () => {
@@ -26,112 +18,32 @@ export const getAllBlogs = () => {
     return blogs
 }
 
-export class BlogCollection {
-    rootPath = ''
-    categories = []
-    tags = []
-    blogs = []
-
-    constructor(rootPath = 'posts') {
-        if (BlogCollection._instance) {
-            return BlogCollection._instance
-        }
-        BlogCollection._instance = this
-        this.rootPath = rootPath
-        this.categories = new Category(rootPath).categories
-        this.blogs = this.getBlogsSortedByDate(rootPath).map((blog) => new Blog(blog))
-        this.tags = this.getTagsWithWeight()
-    }
-
-    getBlogsByCategory(category) {
-        const blogs = this.blogs.filter((blog) => blog.category === category)
-        return serializeContent(blogs)
-    }
-
-    getBlogByCategoryAndId(category, id) {
-        const blog = this.getBlogsByCategory(category).find((post) => post.id === id)
-        return serializeContent(parseMarkdownContent(blog))
-    }
-
-    getBlogFilesByCategory(category) {
-        let dirPath = `${this.rootPath}/${category}`
-        const fileNames = fs.readdirSync(path.join(dirPath))
-        return fileNames.filter((fileName) => fileName.includes('.md')).map((f) => ({fileName: f, category}))
-    }
-
-    getAllBlogFileNamesWithCategory() {
-        return this.categories.map((category) => this.getBlogFilesByCategory(category)).flat(1)
-    }
-
-    getBlogsSortedByDate() {
-        return this.getAllBlogFileNamesWithCategory()
-            .map(({fileName, category}) => this.parseFrontMatter(fileName, category))
-            .sort((a, b) => {
-                return dayjs(a.date).isBefore(dayjs(b.date)) ? 1 : -1
-            })
-    }
-
-    getAllPostPaths(isLinkPath = false) {
-        if (isLinkPath) {
-            return this.blogs.map((post) => ({
-                href: {
-                    pathname: `/blog/post/[cname]/[...slug]`,
-                    query: {
-                        cname: post.category,
-                        slug: [post.id],
-                    },
+export const getAllPostPaths = (blogs, isLinkPath = false) => {
+    if (isLinkPath) {
+        return blogs.map((post) => ({
+            href: {
+                pathname: `/blog/post/[cname]/[...slug]`,
+                query: {
+                    cname: post.category,
+                    slug: [post.id],
                 },
-                id: post.id,
-                title: post.title,
-                as: `/blog/post/${post.category}/${post.id}`,
-            }))
-        }
-        return this.blogs.map((post) => ({
-            params: {
-                cname: post.category,
-                slug: [post.id],
             },
+            id: post.id,
+            title: post.title,
+            as: `/blog/post/${post.category}/${post.id}`,
         }))
     }
-
-    getTagsWithWeight() {
-        const tags = _.map(this.blogs, 'tags').flat(1)
-        return _.values(_.groupBy(tags)).map((tag) => ({
-            text: tag[0],
-            value: tag.length,
-        }))
-    }
-
-    parseFrontMatter(fileName, category) {
-        const slug = fileName.replace(/\.md$/, '')
-        const markdownWithMeta = fs.readFileSync(path.join(`${this.rootPath}/${category}/${fileName}`), 'utf-8')
-        const {data: frontMatter, content} = matter(markdownWithMeta)
-        const {title, date, excerpt, cover = '../cover_placeholder.png', tags = []} = frontMatter
-        const coverImage = cover
-        return {
-            id: slug,
-            title,
-            date,
-            excerpt,
-            content,
-            coverImage,
-            category,
-            tags,
-        }
-    }
+    return blogs.map((post) => ({
+        params: {
+            cname: post.category,
+            slug: [post.id],
+        },
+    }))
 }
 
 const parseMarkdownContent = (blog) => {
     const {parsedContent, tocTree} = getParsedContentWithTocTree(blog.content)
     return {...blog, content: parsedContent, tocTree}
-}
-
-export const serializeContent = (content) => {
-    try {
-        return JSON.parse(JSON.stringify(content))
-    } catch (error) {
-        throw new Error(error)
-    }
 }
 
 export function randomEmoji() {
--- pages/blog/post/[cname]/[...slug].js
+++ pages/blog/post/[cname]/[...slug].js
@@ -2,48 +2,27 @@ import {Box, Container, Flex, Heading, HStack, Image, Tag, Text, VStack} from '@
 import dayjs from 'dayjs'
 import Head from 'next/head'
 import {useRouter} from 'next/router'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import BackToTop from '../../../../components/BackToTop'
 import CustomDivider from '../../../../components/CustomDivider'
 import LinkToPost from '../../../../components/LinkToPost'
 import Toc from '../../../../components/Toc'
-import {randomEmoji} from '../../../../helpers'
+import {getAllBlogs, getAllPostPaths, randomEmoji} from '../../../../helpers'
 import BlogLayout from '../../../../layout/BlogLayout'
-import {getPostDetailsBySlug, getPosts, getTagRelatedPosts} from '../../../../services'
-import {getParsedContentWithTocTree} from '../../../../helpers/markDownRenderer'
-import {RELATED_POST_LABEL} from '../../../../utils/content'
-import {isProduction} from '../../../../helpers/env'
+import {NEXT_POST_LABEL, PREVIOUS_POST_LABEL} from '../../../../utils/content'
 
-const emoji = randomEmoji()
-const getRelatedPostLink = (relatedPosts) =>
-    relatedPosts.map((relatedPost) => {
-        const {categories, slug, id, title, date} = relatedPost
-        const category = categories[0]
-        return {
-            href: {
-                pathname: `/blog/post/[cname]/[...slug]`,
-                query: {
-                    cname: category.slug,
-                    slug: [slug],
-                },
-            },
-            id: id,
-            title: title,
-            as: `/blog/post/${category.slug}/${slug}`,
-            date: date,
-        }
-    })
-
-function Post({post = {}, relatedPosts}) {
+function Post({post = {}, previousPath, nextPath}) {
     const router = useRouter()
-    const {id, title, coverImage, tags, categories = [], parsedContent, tocTree, date} = post
+    const [emoji] = useState(randomEmoji())
+
+    const {id, title, coverImage, tags, category, content, date, tocTree} = post
+
+    // const { cname, slug } = router.searchValue
+    // const id = slug[0]
 
     useEffect(() => {
         !id && router.push('/blog')
     }, [])
-    const category = categories[0] || {}
-
-    const relatedPostLink = getRelatedPostLink(relatedPosts)
 
     return (
         <>
@@ -55,7 +34,7 @@ function Post({post = {}, relatedPosts}) {
             <Container maxW="container.xl" my={8}>
                 <CustomDivider my={4} text={emoji} dividerWidth="full" />
                 <VStack spacing="4">
-                    <Image w={['90%', '80vw', '60vw']} maxW="600px" objectFit="contain" src={coverImage?.url} />
+                    <Image w={['90%', '80vw', '60vw']} maxW="600px" objectFit="contain" src={coverImage} />
                     <HStack spacing={4}>
                         {tags?.map((tag) => (
                             <Tag key={tag}>{tag}</Tag>
@@ -66,8 +45,8 @@ function Post({post = {}, relatedPosts}) {
                     </Box>
 
                     <Flex w={['90%', '80vw', '60vw']} maxW="1000px" gap={4}>
-                        <Text fontSize="0.8rem">{category.name}</Text>
-                        <Text fontSize="0.8rem">{date && dayjs(date).format('MM-DD, YYYY')}</Text>
+                        <Text fontSize="0.8rem">{category}</Text>
+                        <Text fontSize="0.8rem">{dayjs(date).format('MM-DD, YYYY')}</Text>
                     </Flex>
 
                     <Box w={['90%', '80vw', '60vw']} maxW="1000px" className="markdown-body">
@@ -79,20 +58,14 @@ function Post({post = {}, relatedPosts}) {
                         maxW="1000px"
                         className="markdown-body"
                         id="content"
-                        dangerouslySetInnerHTML={{__html: parsedContent}}
+                        dangerouslySetInnerHTML={{__html: content}}
                     />
                     <CustomDivider my={4} text={emoji} dividerWidth="full" />
 
-                    <VStack w={'full'} alignItems={'end'}>
-                        {relatedPostLink.length && (
-                            <>
-                                <Text>{RELATED_POST_LABEL}</Text>
-                                {relatedPostLink.map((payload) => (
-                                    <LinkToPost payload={payload} key={payload.id} />
-                                ))}
-                            </>
-                        )}
-                    </VStack>
+                    <Flex w={['auto', 'full']} justifyContent={['center', 'space-between']} flexDir={['column', 'row']}>
+                        <LinkToPost payload={previousPath} />
+                        <LinkToPost payload={nextPath} />
+                    </Flex>
                 </VStack>
                 <BackToTop />
             </Container>
@@ -103,17 +76,38 @@ function Post({post = {}, relatedPosts}) {
 export async function getStaticProps(context) {
     try {
         const {params} = context
-        const [slug] = params.slug
-        const post = await getPostDetailsBySlug(slug)
-        if (isProduction && post.published === false) {
+        const [id] = params.slug
+
+        const posts = getAllBlogs()
+        const linkPaths = getAllPostPaths(posts, true)
+
+        const post = posts.find((post) => post.id === id)
+
+        if (!post) {
             return {
-                props: {},
+                props: {post: {}},
             }
         }
-        const relatedPosts = await getTagRelatedPosts(slug, post.tags)
-        const {parsedContent, tocTree} = getParsedContentWithTocTree(post?.contentMarkdown)
+
+        const currentPathIndex = linkPaths.findIndex((path) => path.id === id)
+
+        let previousPath = null,
+            nextPath = null
+        if (currentPathIndex !== 0) {
+            previousPath = linkPaths[currentPathIndex - 1]
+            previousPath.label = PREVIOUS_POST_LABEL
+        }
+        if (currentPathIndex !== linkPaths.length - 1) {
+            nextPath = linkPaths[currentPathIndex + 1]
+            nextPath.label = NEXT_POST_LABEL
+        }
+
         return {
-            props: {post: {...post, parsedContent, tocTree}, relatedPosts},
+            props: {
+                post,
+                previousPath,
+                nextPath,
+            },
         }
     } catch (error) {
         return {
@@ -127,23 +121,16 @@ export async function getStaticProps(context) {
 
 export async function getStaticPaths() {
     try {
-        let posts = await getPosts()
-        posts = isProduction ? posts.filter((post) => post.published === true) : posts
-        const paths = posts.map(({slug, categories}) => ({
-            params: {
-                cname: categories[0]?.slug,
-                slug: [slug],
-            },
-        }))
-
+        const posts = getAllBlogs()
+        const paths = getAllPostPaths(posts)
         return {
-            paths: paths,
+            paths,
             fallback: false,
         }
     } catch (error) {
         return {
             paths: [],
-            fallback: true,
+            fallback: false,
         }
     }
 }
