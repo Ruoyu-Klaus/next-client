d723ddac16303c6b134a19a5672ab2959ce08d93
ruoyuklaus <klaus1201810802@gmail.com>
2022-05-09 23:18:56 +0800

    :recycle: refactor: clean code

--- hooks/useRouterScroll.js
+++ hooks/useRouterScroll.js
@@ -1,25 +1,25 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import {useRouter} from 'next/router'
+import {useEffect} from 'react'
 
-function useRouterScroll({ behavior = 'smooth', left = 0, top = 0 } = {}) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChangeComplete = url => {
-      window.scrollTo({ top, left, behavior });
-      process.env === 'production' &&
-        window.gtag('config', 'G-TVHYT0JX36', {
-          page_path: url,
-        });
-    };
+function useRouterScroll({behavior = 'smooth', left = 0, top = 0} = {}) {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChangeComplete = (url) => {
+            window.scrollTo({top, left, behavior})
+            process.env.NODE_ENV === 'production' &&
+                window.gtag('config', 'G-TVHYT0JX36', {
+                    page_path: url,
+                })
+        }
 
-    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+        router.events.on('routeChangeComplete', handleRouteChangeComplete)
 
-    // If the component is unmounted, unsubscribe from the event
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChangeComplete);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [behavior, left, top]);
+        // If the component is unmounted, unsubscribe from the event
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChangeComplete)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [behavior, left, top])
 }
 
-export default useRouterScroll;
+export default useRouterScroll
