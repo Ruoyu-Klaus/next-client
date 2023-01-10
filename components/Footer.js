5e5f0b94a23fff2d14e7ca21e8896d92cab28f5c
Ruoyu <klaus1201810802@gmail.com>
2023-01-10 23:24:09 +0800

    ✨feat: update copyright for footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,14 @@
-import {Box, Link, Text, VStack} from '@chakra-ui/react'
+import { Box, Link, Text, VStack } from "@chakra-ui/react";
+import dayjs from "dayjs";
 
 const Footer = (props) => (
     <Box {...props}>
         <VStack className="footer-div">
             <Text fontSize="0.5rem">
-                <Link href="https://ruoyu.life">ruoyu.life</Link>
+                <Link href="https://ruoyu.life">ruoyu.life Made by Ruoyu Klaus</Link>
             </Text>
             <Text fontSize="0.5rem">
-                Copyright © 2022 <Link href="https://ruoyu.life">ruoyu.life All rights reserved.</Link>
+                Copyright © {dayjs().year()} <Link href="https://ruoyu.life">ruoyu.life All rights reserved.</Link>
             </Text>
         </VStack>
     </Box>
