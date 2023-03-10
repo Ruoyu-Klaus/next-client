a9fd3a70440027f55dab7aade86eae12d14b6999
Ruoyu <klaus1201810802@gmail.com>
2023-03-10 12:06:41 +0800

    💉fix: build issue

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,24 @@
 import Document, {Head, Html, Main, NextScript} from 'next/document'
 
+const isProduction = process.env.NODE_ENV === 'production'
 class MyDocument extends Document {
     render() {
         return (
             <Html lang="zh-CN">
                 <Head>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TVHYT0JX36"></script>
-                    <script>
-                        window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
-                        gtag('js', new Date()); gtag('config', 'G-TVHYT0JX36');
-                    </script>
+                    {isProduction && (
+                        <>
+                            <script async src="https://www.googletagmanager.com/gtag/js?id=G-TVHYT0JX36"></script>
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: ` window.dataLayer = window.dataLayer || []; 
+                                              function gtag(){dataLayer.push(arguments)}
+                                              gtag('js', new Date()); 
+                                              gtag('config', 'G-TVHYT0JX36');`,
+                                }}
+                            ></script>
+                        </>
+                    )}
                     <title>Home | 首页</title>
                 </Head>
                 <body>
