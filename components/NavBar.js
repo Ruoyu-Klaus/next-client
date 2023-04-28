e44f903d14c86ce0722225ebef17cf38a6c11caa
Ruoyu <klaus1201810802@gmail.com>
2023-04-28 15:43:24 +0800

    💄 feat: responsive header

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,39 +1,42 @@
 import PropTypes from 'prop-types'
-import React, {useCallback, useEffect, useState} from 'react'
+import React, {useCallback, useState} from 'react'
 
 import MenuItemLink from './MenuItemLink'
-import NavBarLogo from './NavBarLogo'
-import MenuToggleForSmallScreen from './NavBarMenuToggle'
 
 import {SearchIcon} from '@chakra-ui/icons'
-import {Box, Text, Button, Grid, GridItem, HStack, Stack, useColorMode, useColorModeValue} from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    Grid,
+    GridItem,
+    HStack,
+    Popover,
+    PopoverBody,
+    PopoverContent,
+    PopoverTrigger,
+    Text,
+    VStack,
+    useColorMode,
+    useMediaQuery,
+} from '@chakra-ui/react'
 import useRouterScroll from '../hooks/useRouterScroll'
-import {NAVIGATION_ABOUTPAGE, NAVIGATION_HOMEPAGE, LIGHT_MODE_ICON, DARK_MODE_ICON} from '../utils/content'
+import {DARK_MODE_ICON, LIGHT_MODE_ICON, NAVIGATION_ABOUTPAGE, NAVIGATION_HOMEPAGE} from '../utils/content'
+
+import {CloseIcon, HamburgerIcon} from '@chakra-ui/icons'
+import NextImage from 'next/image'
+import Link from 'next/link'
 
 Header.propTypes = {
     navArray: PropTypes.array,
 }
 
 function Header({navArray = []}) {
-    const {colorMode, toggleColorMode} = useColorMode()
     useRouterScroll()
+    const {colorMode, toggleColorMode} = useColorMode()
+    const [isLargerThan900] = useMediaQuery('(min-width: 900px)')
 
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     const toggleMenuIcon = () => setIsMenuOpen(!isMenuOpen)
-
-    useEffect(() => {
-        colorMode === 'dark' ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
-    }, [])
-
-    const handleThemeChange = useCallback(
-        (e) => {
-            toggleColorMode()
-            e.preventDefault()
-            e.stopPropagation()
-            document.body.classList.toggle('dark-mode')
-        },
-        [colorMode],
-    )
 
     const renderCategoriesLink = useCallback(
         () =>
@@ -48,26 +51,36 @@ function Header({navArray = []}) {
     )
 
     return (
-        <Grid margin="0 auto" w="80%" maxW="container.xl" h={16} px={8} mt={8} templateColumns="repeat(2, 1fr)" alignItems={'center'}>
-            <HStack spacing={{base: 'none', md: 4}}>
-                <NavBarLogo w="80%" m="0 auto" />
+        <Grid
+            margin="0 auto"
+            w="80%"
+            maxW="container.xl"
+            h={16}
+            px={isLargerThan900 ? 8 : 0}
+            mt={8}
+            templateColumns="repeat(2, 1fr)"
+            alignItems={'center'}
+        >
+            <HStack w={isLargerThan900 ? '80%' : '60%'} m="0 auto">
+                <Link href={{pathname: '/'}} as={'/'} passHref>
+                    <a title={'home'}>
+                        <Box w={{base: 100, md: 300}} h="50" pos="relative">
+                            <NextImage
+                                overflow="hidden"
+                                alt="klauswang"
+                                layout="fill"
+                                objectFit="scale-down"
+                                priority={true}
+                                src={colorMode === 'light' ? '/klauswang.png' : '/klauswang-white.png'}
+                            />
+                        </Box>
+                    </a>
+                </Link>
             </HStack>
 
             <GridItem justifySelf="end">
-                <MenuToggleForSmallScreen display={{base: 'block', md: 'none'}} toggle={toggleMenuIcon} isMenuOpen={isMenuOpen} />
-                <Box
-                    display={{base: isMenuOpen ? 'flex' : 'none', md: 'flex'}}
-                    top={16}
-                    right={0}
-                    zIndex={20}
-                    w={{base: '100vw', md: 'auto'}}
-                    pos={{base: 'fixed', md: 'unset'}}
-                    bg={{
-                        base: useColorModeValue('gray.200', 'gray.900'),
-                        md: 'transparent',
-                    }}
-                >
-                    <Stack py={{base: 4, md: 'inherit'}} w="full" alignItems={'center'} spacing="4" direction={['column', 'column', 'row']}>
+                {isLargerThan900 ? (
+                    <HStack w="full" alignItems={'center'} spacing={4}>
                         <MenuItemLink to="/blog">
                             <Button variant="link">{NAVIGATION_HOMEPAGE}</Button>
                         </MenuItemLink>
@@ -80,11 +93,42 @@ function Header({navArray = []}) {
                                 <SearchIcon />
                             </Button>
                         </MenuItemLink>
-                        <Button variant="ghost" onClick={handleThemeChange}>
+                        <Button variant="ghost" onClick={toggleColorMode}>
                             {colorMode === 'light' ? <Text fontSize="lg">{DARK_MODE_ICON}</Text> : <Text fontSize="lg">{LIGHT_MODE_ICON}</Text>}
                         </Button>
-                    </Stack>
-                </Box>
+                    </HStack>
+                ) : (
+                    <HStack spacing={0}>
+                        <Button variant="ghost" onClick={toggleColorMode}>
+                            {colorMode === 'light' ? <Text fontSize="lg">{DARK_MODE_ICON}</Text> : <Text fontSize="lg">{LIGHT_MODE_ICON}</Text>}
+                        </Button>
+                        <MenuItemLink to="/blog/search">
+                            <Button variant="ghost">
+                                <SearchIcon />
+                            </Button>
+                        </MenuItemLink>
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button variant="ghost" onClick={toggleMenuIcon}>
+                                    {isMenuOpen ? <CloseIcon /> : <HamburgerIcon />}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent w="100px">
+                                <PopoverBody>
+                                    <VStack>
+                                        <MenuItemLink to="/blog">
+                                            <Button variant="link">{NAVIGATION_HOMEPAGE}</Button>
+                                        </MenuItemLink>
+                                        {renderCategoriesLink()}
+                                        <MenuItemLink to="/about">
+                                            <Button variant="link">{NAVIGATION_ABOUTPAGE}</Button>
+                                        </MenuItemLink>
+                                    </VStack>
+                                </PopoverBody>
+                            </PopoverContent>
+                        </Popover>
+                    </HStack>
+                )}
             </GridItem>
         </Grid>
     )
